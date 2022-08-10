8dde9ed2775252a2155bdac0aaffdc2256b35f69
ashraf-a-khan <khan.ashraf2311@gmail.com>
2022-08-10 00:02:24 -0400

    Added MajorityElement problem.

---
+++ MajorityElement.js
@@ -0,0 +1,21 @@
+// 169. Majority Element
+
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times.
+// You may assume that the majority element always exists in the array.
+
+var majorityElement = function (nums) {
+  let count = {};
+  for (const element of nums) {
+    if (count[element]) {
+      count[element] += 1;
+    } else {
+      count[element] = 1;
+    }
+  }
+  let maximum = Object.keys(count).reduce((a, b) =>
+    count[a] > count[b] ? a : b
+  );
+  return maximum;
+};
--- TwoSum.js
+++ TwoSum.js
@@ -1,3 +1,11 @@
+// 1. Two Sum
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
 let nums = [3, 2, 4];
 let target = 6;
 
@@ -15,6 +23,3 @@ var twoSum = function (nums, target) {
     i++;
   }
 };
-
-// twoSum(nums, target);
-console.log(twoSum(nums, target));
